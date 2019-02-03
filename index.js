207569e7f92e05d64ca6283443964f6a74721451
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-02-03 23:13:22 +0200

    Installed express server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = require('express')();
+var http = require('http').Server(app);
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('client'))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+http.listen(PORT, function(){
+    console.log('listening on *:4000');
+});
