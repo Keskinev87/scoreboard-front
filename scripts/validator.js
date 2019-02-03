9bfbe3b6c5743587fee62b2193597bb85c86983e
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-02-03 22:51:14 +0200

    Fixed some regexes

--- scripts/validator.js
+++ scripts/validator.js
@@ -4,7 +4,8 @@ class Validator {
         this.nameRegex = /^[a-z\d\s]+$/i;
         this.countryMaxChar = 50;
         this.countryRegex = /^[a-z\d\s]+$/i;
-        this.dateRegex = /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/;
+        this.dateRegex = /\d{4}-[0-1]\d-[0-3]\d\)?/gm;
+        // this.dateRegex = /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/;
         this.scoreRegex = /^[0-9]*$/;
     }
 
@@ -44,6 +45,8 @@ class Validator {
         let reg = new RegExp(this.scoreRegex);
 
         if(!reg.test(score))
+            return false;
+        else if(score > 100)
             return false;
         else 
             return true;
