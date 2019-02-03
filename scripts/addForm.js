2ee085bd44c5542b60876b473f18de2ff8231607
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-02-03 22:52:18 +0200

    Small fixes of the validation

--- scripts/addForm.js
+++ scripts/addForm.js
@@ -1,19 +1,19 @@
 class AddForm {
     constructor(){
-        this.countries = ["Afghanistan", "Algeria"];
         this.form = document.getElementById("add-form");
         this.countryOptions = document.getElementById('playersCountry');
         this.nameFormGroup = document.getElementById('add-name-group');
         this.countryFormGroup = document.getElementById('add-country-group')
         this.scoreFormGroup = document.getElementById('add-score-group'); 
         this.closeBtn = document.getElementById("add-form-close-btn");
+        this.errorMsg = document.getElementById('err-msg');
         this.nameError = false;
         this.countryError = false;
         this.scoreError = false;
     }
 
     populateCountries() {
-        for (let country of this.countries){
+        for (let country of countries){
             let countryOption = document.createElement('option');
             countryOption.innerHTML = country;
             this.countryOptions.appendChild(countryOption);
@@ -97,7 +97,7 @@ class AddForm {
                 this.displayError("Server error. Please contact support", this.form)
                 break;
             default:
-                this.displayError("No connection to the server. Please check your connection", this.form)
+                this.displayError("No connection to the server. Please check your connection and retry. If the problem persists, please contact support!", this.form)
                 break;
         }
     
@@ -108,42 +108,30 @@ class AddForm {
             case 'name':
                 let playersName = document.getElementById('playersName').value;
                 if(!validator.validateName(playersName)) {
-                    if(this.nameError == false)
-                        this.displayError("Names must be under 80 characters and include only numbers and digits!", this.nameFormGroup);
-
+                    this.displayError("Names must be under 80 characters and include only numbers and digits!", this.nameFormGroup);
                     this.nameError = true;
                 } else {
-                    if(this.nameError)
-                        this.removeError(this.nameFormGroup);
-
+                    this.removeError(this.nameFormGroup);
                     this.nameError = false;
                 }
                 break;
             case 'country':
                 let playersCountry = document.getElementById('playersCountry').value;
                 if(!validator.validateCountry(playersCountry)) {
-                    if(this.countryError == false)
-                        this.displayError("Country names must be under 50 characters and may include only numbers and digits!", this.countryFormGroup)
-
+                    this.displayError("Country names must be under 50 characters and may include only numbers and digits!", this.countryFormGroup)
                     this.countryError = true;
                 } else {
-                    if(this.countryError)
-                        this.removeError(this.countryFormGroup);
-
+                    this.removeError(this.countryFormGroup);
                     this.countryError = false;
                 }
                 break;
             case 'score':
                 let playersScore = document.getElementById('playersScore').value;
                 if(!validator.validateScore(playersScore)) {
-                    if(this.countryError == false)
-                        this.displayError("The score must be a positive number!", this.scoreFormGroup)
-                    
-                    this.countryError = true;
+                    this.displayError("The score must be a positive number less than 100!", this.scoreFormGroup)
+                    this.scoreError = true;
                 } else {
-                    if(this.scoreError)
-                        this.removeError(this.scoreFormGroup);
-                    
+                    this.removeError();
                     this.scoreError = false;
                 }
                 break;
@@ -153,15 +141,14 @@ class AddForm {
     }
 
     displayError(msg, target) {
-        let errorMsg = document.getElementById('err-msg');
-        errorMsg.style.visibility = '';
-        errorMsg.innerHTML = msg;
-        target.appendChild(errorMsg)
+        
+        this.errorMsg.style.visibility = '';
+        this.errorMsg.innerHTML = msg;
+        target.appendChild(this.errorMsg)
     }
 
-    removeError(target) {
-        let errorMsg = document.getElementById('err-msg');
-        errorMsg.style.visibility = 'hidden'
+    removeError() {
+        this.errorMsg.style.visibility = 'hidden'
     }
 
 }
