b98b72724f509f3441708e3598ea2177aaeabdc6
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-02-03 22:53:45 +0200

    loading players now sends the filter options to the server

--- scripts/scoreboard.js
+++ scripts/scoreboard.js
@@ -4,30 +4,34 @@ class ScoreBoard {
         this.players=[];
         this.loader = document.getElementById("loader");
         this.scoreBoard = document.getElementById('scoreboard');
+        this.errorMsg = document.getElementById('scoreboard-error');
     }
 
-    async loadPlayers () {
+    async loadPlayers (filterName, filterCountries, filterDateFrom, filterDateTo, sortCriteria, sortType) {
+        this.scoreBoard.innerHTML='';
+        console.log(filterName, filterCountries, filterDateFrom)
         let url = new URL(BACKEND_URL + '/players');
         let params= {
-            name:'',
-            countries:[],
-            registrationFrom: null,
-            registrationTo: null,
-            filterCriteria: '',
-            filterType:''
+            filterName: filterName || '',
+            filterCountries: filterCountries || [],
+            filterDateFrom: filterDateFrom || '',
+            filterDateTo: filterDateTo || '',
+            sortCriteria: sortCriteria || '',
+            sortType: sortType || ''
         };
         let queryOptions = {
-            method: "GET",
+            method: "POST",
             mode: "cors",
             cache: "no-cache",
             headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
+                "Content-Type": "application/json"
             },
-            redirect: "follow"
+            redirect: "follow",
+            body: JSON.stringify(params)
         };
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    
-    
+        // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+        
+        this.loader.style.display = '';
         let data = await fetch(url, queryOptions)
             .then(function(response) {
             return response.json();
@@ -35,32 +39,46 @@ class ScoreBoard {
             .then(function(responseData) {
                 return responseData;
             })
-            .catch(error => console.error(error));
+            .catch(error => {
+                this.loader.style.display = 'none';
+                this.displayError("No connection to the server. Please check your internet connection and refresh the page! If the problem persists, please contact support!");
+            });
 
-        console.log(data)
-        this.players=data;
-        this.loader.style.display = 'none';
-        this.createTable(this.players);
+        if(data){
+            this.players=data;
+            this.loader.style.display = 'none';
+            this.errorMsg.style.display = 'none';
+            this.createTable(this.players);
+        }
+        
     }
     
     createTable(arr) {
-        arr.forEach(element => {
+        arr.forEach((element, index) => {
             let tr = document.createElement('tr');
+            let tdRank = document.createElement('td');
+            tdRank.innerText = index + 1;
             let tdName = document.createElement('td');
             tdName.innerText = element.name;
             let tdCountry = document.createElement('td');
-            tdCountry.innerText = element.country;
+            tdCountry.innerText = element.country.toUpperCase();
             let tdDate = document.createElement('td');
             tdDate.innerText = new Date(element.registrationDate).toLocaleDateString();
             let tdScore = document.createElement('td');
             tdScore.innerText = element.score;
-    
+            
+            tr.appendChild(tdRank);
             tr.appendChild(tdName);
             tr.appendChild(tdCountry);
             tr.appendChild(tdDate);
             tr.appendChild(tdScore);
             this.scoreBoard.appendChild(tr);
         });
+    }
+
+    displayError(msg) {
+        this.errorMsg.innerHTML = msg;
+        this.errorMsg.style.display = '';
     }
     
     clearTable() {
